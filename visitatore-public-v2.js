10b3726c1fa18b5b44e259b1af5b2d52147481d5
josriz <giose.rizzi@gmail.com>
2026-09-25 13:13:16 +0200

    Ripristina visitatore al Supabase storico

--- visitatore-public-v2.js
+++ visitatore-public-v2.js
@@ -3,8 +3,8 @@
 */
 (function(){
 'use strict';
-const URL_SUPABASE='https://dkeqicstprvvfebiaooc.supabase.co';
-const KEY='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRrZXFpY3N0cHJ2dmZiaWFvY29pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjExNjQ2NjksImV4cCI6MjA3NjQwNjY5fQ.MPlE8CZ2B8pEhSJzTWBJ-FfFLQpwRAxlAlno-SCylWg';
+const URL_SUPABASE='https://iybjvtmfaupgthqqsngd.supabase.co';
+const KEY='sb_publishable_oLLML3_ne0I1dWKIinSRNA_K1Ao5SOl';
 let client=null, tournaments=[], counts={};
 const esc=v=>String(v??'').replace(/[&<>"']/g,m=>({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#39;'}[m]));
 const id=v=>Number(v)||0;
